944e9269a083073bf30b759abf8ff6330f1dc11f
fand <fand.gmork@gmail.com>
2019-11-17 02:57:45 +0900

    Move eye target

--- Assets/FaceController.cs
+++ Assets/FaceController.cs
@@ -13,6 +13,7 @@ public class FaceController : MonoBehaviour
     [SerializeField] Transform _root;
     [SerializeField] Transform _neck;
     [SerializeField] Transform _eyeTarget;
+    [SerializeField] Transform _camera;
     uOscServer _osc;
     float _smile;
     float _smileL;
@@ -22,6 +23,8 @@ public class FaceController : MonoBehaviour
     float _sad;
     float _surprise;
     float _fun;
+
+    Vector2 _targetBias = Vector2.zero;
 
     void Start()
     {
@@ -36,18 +39,21 @@ public class FaceController : MonoBehaviour
 
         var args = message.values.Select(x => (float)x).ToList();
 
+        // face transform
         if (adr == "faceposition")
         {
-            _root.position = new Vector3(-args[0], args[1], args[2]);
+            _root.position = Vector3.Lerp(_root.position, new Vector3(-args[0], args[1], args[2] * 3), 0.1f);
         }
         else if (adr == "facerotation")
         {
             var rot = new Quaternion(args[0], args[1], args[2], args[3]);
             var euler = rot.eulerAngles;
-            _neck.rotation = Quaternion.Euler(euler.x, -euler.y, -euler.z);
+            _neck.rotation = Quaternion.Slerp(_neck.rotation, Quaternion.Euler(euler.x, -euler.y, -euler.z), 0.3f);
             euler *= 0.02f;
-            _root.rotation = Quaternion.Slerp(_root.rotation, Quaternion.Euler(-euler.x, euler.y, euler.z), 0.1f);
+            _root.rotation = Quaternion.Slerp(_root.rotation, Quaternion.Euler(-euler.x, euler.y, euler.z), 0.03f);
         }
+
+        // face blendshapes
         else if (adr == "faceeyeblinkleft")
         {
             var x = args[0] * 1.4f;
@@ -88,6 +94,24 @@ public class FaceController : MonoBehaviour
         {
             _fun = Mathf.Lerp(_fun, args[0], 0.2f);
         }
+
+        // eye movement
+        else if (adr == "faceeyelookinleft" || adr == "faceeyelookoutright")
+        {
+            _targetBias.x = Mathf.Lerp(_targetBias.x, args[0], 0.2f);
+        }
+        else if (adr == "faceeyelookoutleft" || adr == "faceeyelookinright")
+        {
+            _targetBias.x = Mathf.Lerp(_targetBias.x, -args[0], 0.2f);
+        }
+        else if (adr == "faceeyelookupleft" || adr == "faceeyelookupright")
+        {
+            _targetBias.y = Mathf.Lerp(_targetBias.y, args[0], 0.2f);
+        }
+        else if (adr == "faceeyelookdownleft" || adr == "faceeyelookdownright")
+        {
+            _targetBias.y = Mathf.Lerp(_targetBias.y, -args[0], 0.2f);
+        }
     }
 
     void Update()
@@ -105,6 +129,14 @@ public class FaceController : MonoBehaviour
         Face("mouth_shock", _surprise * 2 - 1);
 
         Anim("FUN", _fun * 2);
+
+        // move eye target
+        var targetDiff = _neck.rotation * Quaternion.Euler(_targetBias.y * -120, _targetBias.x * 120, 0) * Vector3.forward;
+        var eyePoint = _neck.transform.position + Vector3.up * 0.15f;
+        _eyeTarget.position = eyePoint + targetDiff;
+
+        // Set camera target to neck
+        _camera.rotation = Quaternion.Slerp(_camera.rotation, Quaternion.LookRotation(eyePoint - _camera.position, _camera.up), 0.02f);
     }
 
     void Anim(string name, float value)
